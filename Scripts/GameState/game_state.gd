548f2cd7e5664b2688d441ccb79bd7dc1d5d70af
nthung-2k5 <hungthitkhia@gmail.com>
2023-09-02 17:17:41 +0700

    Add footstep soundbox and basic game state

---
+++ Scripts/GameState/game_state.gd
@@ -0,0 +1,29 @@
+extends Node
+
+# 4 loại enum (dừng, đang chơi, thắng, thua)
+enum { PAUSE, ACTIVE, WIN, LOSE }
+enum { MENU, STAGE }
+var current_state = ACTIVE
+var current_scene = MENU
+
+var main_stage_node # phải nhớ gán vào
+
+signal stage_won
+signal stage_lose
+
+func _process(_delta):
+    if Input.is_action_just_pressed("ui_pause") && current_scene == STAGE && current_state not in [WIN, LOSE]:
+        get_tree().paused = !get_tree().paused
+        current_state = PAUSE if get_tree().paused else ACTIVE
+
+func set_won():
+    current_state = WIN
+    main_stage_node.process_mode = PROCESS_MODE_DISABLED
+
+func set_lose():
+    current_state = LOSE
+    main_stage_node.process_mode = PROCESS_MODE_DISABLED
+
+func reset():
+    current_state = ACTIVE
+    main_stage_node.process_mode = PROCESS_MODE_INHERIT
--- Scripts/Player/player.gd
+++ Scripts/Player/player.gd
@@ -7,6 +7,8 @@ signal player_respawn
 @export var speed = 400
 @export var dmk_speed = 10
 @export var dmk_damage = 50
+@export var hitbox_radius = 10
+@export var footstep_radius = 20
 
 @onready var animation_tree : AnimationTree = $AnimationTree
 @onready var fire_position : Marker2D = $FirePosition
@@ -14,17 +16,21 @@ signal player_respawn
 var health : int = 100
 var respawns_left : int = 5
 
+func _ready():
+	$Hurtbox/Shape.shape.radius = hitbox_radius
+	$Footstep.max_distance = footstep_radius
+
 # Lấy vector input người dùng để di chuyểm
 func get_input():
 	var input_direction = Input.get_vector("left", "right", "up", "down")
 	velocity = input_direction * speed
 
 # Update animation mỗi frame
-func _process(delta):
+func _process(_delta):
 	update_animation_params()
 	
 # Di chuyển theo thời gian
-func _physics_process(delta):
+func _physics_process(_delta):
 	get_input()
 	look_at(get_global_mouse_position())
 	move_and_slide()
@@ -61,3 +67,8 @@ func take_damage(dmg):
 
 func attack():
 	get_parent().spawn_danmaku(fire_position.position, get_global_mouse_position() - position, dmk_speed, dmk_damage)
+
+
+func _on_footstep_finished():
+	if velocity != Vector2.ZERO: # đang đi
+		$Footstep.play()
