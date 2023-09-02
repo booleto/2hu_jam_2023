0948ad01eea9b829fc182aca1fa111e540776818
booleto <exaccxd@gmail.com>
2023-09-02 17:02:40 +0700

    added dmk sprites and adjusted hitboxes

--- Scripts/Player/player.gd
+++ Scripts/Player/player.gd
@@ -59,5 +59,6 @@ func take_damage(dmg):
 	health -= dmg
 	print("Took damage. Health:", health)
 
+# Tấn công
 func attack():
-	get_parent().spawn_danmaku(fire_position.position, get_global_mouse_position() - position, dmk_speed, dmk_damage)
+	get_parent().spawn_danmaku(fire_position.global_position, get_global_mouse_position() - position, dmk_damage, dmk_speed)
